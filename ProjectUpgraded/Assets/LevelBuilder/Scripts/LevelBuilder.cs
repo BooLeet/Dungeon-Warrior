bf37d5b7ec5abe2c858cdbd91657e63c3303bfac
Maksim Chmutov <maxtheboo1337@gmail.com>
2023-02-23 00:36:48 +0500

    Migrated to HDRP

--- ProjectUpgraded/Assets/LevelBuilder/Scripts/LevelBuilder.cs
+++ ProjectUpgraded/Assets/LevelBuilder/Scripts/LevelBuilder.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class LevelBuilder : MonoBehaviour {
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class LevelBuilder : MonoBehaviour
+{
     public struct LayoutObject
     {
         public enum PieceType { Simple, Wall, Door }
@@ -36,12 +39,12 @@ public class LevelBuilder : MonoBehaviour {
             for (int i = 0; i < map.height; ++i)
                 for (int j = 0; j < map.width; ++j)
                 {
-                    if(map.GetPixel(i,j) == piece.mapColor)
+                    if (map.GetPixel(i, j) == piece.mapColor)
                     {
                         result[i, j].type = piece.type;
                         if (piece.prefabs.Length > 0)
                             result[i, j].prefab = piece.prefabs[Random.Range(0, piece.prefabs.Length)];
-                        if(piece.optionalPrefabs != null && piece.optionalPrefabs.Length > 0)
+                        if (piece.optionalPrefabs != null && piece.optionalPrefabs.Length > 0)
                             result[i, j].optionalPrefab = piece.optionalPrefabs[Random.Range(0, piece.optionalPrefabs.Length)];
                     }
                 }
@@ -54,26 +57,26 @@ public class LevelBuilder : MonoBehaviour {
     {
         for (int i = 0; i < layout.GetLength(0); ++i)
             for (int j = 0; j < layout.GetLength(1); ++j)
-                switch (layout[i,j].type)
+                switch (layout[i, j].type)
                 {
                     case LayoutObject.PieceType.Simple:
-                        InstantiatePiece(layout[i,j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j), Quaternion.identity);
+                        InstantiatePiece(layout[i, j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j), Quaternion.identity);
                         break;
                     case LayoutObject.PieceType.Wall:
-                        if(layout[i,j].optionalPrefab != null)
+                        if (layout[i, j].optionalPrefab != null)
                             InstantiatePiece(layout[i, j].optionalPrefab, blockWidth, blockWidth * new Vector3(i, 0, j), Quaternion.identity);
 
-                        if (i + 1 < layout.GetLength(0) && layout[i + 1,j].type != LayoutObject.PieceType.Wall) 
-                            InstantiatePiece(layout[i,j].prefab, blockWidth, position + blockWidth * new Vector3(i + 0.5f, 0, j), Quaternion.Euler(0, 90, 0));
+                        if (i + 1 < layout.GetLength(0) && layout[i + 1, j].type != LayoutObject.PieceType.Wall)
+                            InstantiatePiece(layout[i, j].prefab, blockWidth, position + blockWidth * new Vector3(i + 0.5f, 0, j), Quaternion.Euler(0, 90, 0));
 
-                        if (i > 0 && layout[i - 1,j].type != LayoutObject.PieceType.Wall) 
-                            InstantiatePiece(layout[i,j].prefab, blockWidth, position + blockWidth * new Vector3(i - 0.5f, 0, j), Quaternion.Euler(0, -90, 0));
+                        if (i > 0 && layout[i - 1, j].type != LayoutObject.PieceType.Wall)
+                            InstantiatePiece(layout[i, j].prefab, blockWidth, position + blockWidth * new Vector3(i - 0.5f, 0, j), Quaternion.Euler(0, -90, 0));
 
-                        if (j + 1 < layout.GetLength(1) && layout[i,j + 1].type != LayoutObject.PieceType.Wall) 
-                            InstantiatePiece(layout[i,j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j + 0.5f), Quaternion.Euler(0, 0, 0));
+                        if (j + 1 < layout.GetLength(1) && layout[i, j + 1].type != LayoutObject.PieceType.Wall)
+                            InstantiatePiece(layout[i, j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j + 0.5f), Quaternion.Euler(0, 0, 0));
 
-                        if (j > 0 && layout[i,j - 1].type != LayoutObject.PieceType.Wall) 
-                            InstantiatePiece(layout[i,j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j - 0.5f), Quaternion.Euler(0, 180, 0));
+                        if (j > 0 && layout[i, j - 1].type != LayoutObject.PieceType.Wall)
+                            InstantiatePiece(layout[i, j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j - 0.5f), Quaternion.Euler(0, 180, 0));
                         break;
                     case LayoutObject.PieceType.Door:
                         InstantiatePiece(layout[i, j].prefab, blockWidth, position + blockWidth * new Vector3(i, 0, j), Quaternion.identity);
@@ -94,7 +97,21 @@ public class LevelBuilder : MonoBehaviour {
     {
         if (prefab == null)
             return;
-        GameObject piece = Instantiate(prefab, position, rotation,transform);
+
+        GameObject piece;
+        if (Application.isPlaying)
+        {
+            piece = Instantiate(prefab, position, rotation, transform);
+        }
+        else
+        {
+#if UNITY_EDITOR
+            piece = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+            piece.transform.position = position;
+            piece.transform.rotation = rotation;
+            piece.transform.parent = transform;
+#endif
+        }
         //LevelPieceProcedure procedure = piece.GetComponent<LevelPieceProcedure>();
         //if (procedure)
         //    procedure.Procedure();
